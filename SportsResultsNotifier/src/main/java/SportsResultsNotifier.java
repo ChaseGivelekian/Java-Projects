4471fa7e5e0e8517c1b83cfa6a222db0ee1704ac
Chase Givelekian <mc_cgivelekian25@mcjvs2.org>
2025-02-26 14:22:16 -0500

    Finished the SportsResultsNotifier project.

--- SportsResultsNotifier/src/main/java/SportsResultsNotifier.java
+++ SportsResultsNotifier/src/main/java/SportsResultsNotifier.java
@@ -1,19 +1,22 @@
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class SportsResultsNotifier {
+    private static final String username = "givelekianc@gmail.com";
+
     public static void main(String[] args) {
-        // Website URL
         String url = "https://www.basketball-reference.com/boxscores/";
         Document doc = getWebsiteResponse(url);
         List<String> gameResults = getGameResults(doc);
-        System.out.println(gameResults);
-//        sendEmail(gameResults);
+        sendEmail(gameResults);
     }
 
     private static Document getWebsiteResponse(String url) {
@@ -25,24 +28,53 @@ public class SportsResultsNotifier {
     }
 
     private static List<String> getGameResults(Document doc) {
-        var gameSummaries = doc.getAllElements().stream().filter(
-                element -> element.tagName().equals("div") && element.className().equals("game_summaries")
-        ).toList();
+        var gameSummaries = doc.select("div.game_summaries");
 
         List<String> results = new ArrayList<>();
 
-        for (Element gameSummary : gameSummaries) {
-            String winner = gameSummary.getElementsByClass("winner").select("td a").text();
+        for (var gameSummary : gameSummaries.select("div.game_summary")) {
+            String winner = gameSummary.getElementsByClass("winner").select("td a").getFirst().text();
             String winnerScore = gameSummary.getElementsByClass("winner").select("td").get(1).text();
-            String loser = gameSummary.getElementsByClass("loser").select("td a").text();
+            String loser = gameSummary.getElementsByClass("loser").select("td a").getFirst().text();
             String loserScore = gameSummary.getElementsByClass("loser").select("td").get(1).text();
-            String formattedResult = "In the game between %s and %s, %s won with a score of %s to %s's %s".formatted(winner, loser, winner, winnerScore, loser, loserScore);
+            String formattedResult = "In the game between %s and %s, %s won with a score of %s to %s's %s.".formatted(winner, loser, winner, winnerScore, loser, loserScore);
             results.add(formattedResult);
         }
         return results;
     }
 
     private static void sendEmail(List<String> gameResults) {
-        // Implement email sending logic here
+        Session session = getSession();
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(username));
+            message.setSubject("Today's NBA Results");
+            message.setText("Here are the results of last night's games:\n\n" + String.join("\n\n", gameResults));
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Session getSession() {
+        try {
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+
+            return Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, System.getenv("GMAIL_PASSWORD"));
+                }
+            });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
